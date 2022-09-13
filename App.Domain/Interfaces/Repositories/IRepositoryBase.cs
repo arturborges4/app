bdcbbc0d0acc7d9ba1094e29db7ed692e0149803
unknown <Administrador@LAB-COMP-02.urisan.lab>
2022-09-13 17:32:07 -0300

    sdasd

---
+++ App.Domain/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> where);
+        void save(TEntity obj);
+        void update(TEntity obj);
+        int SaveChanges();
+        DbContext Context();
+    }
+}
